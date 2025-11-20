f10d658c841682a45aec8812e561953aa4e01fca
shayan abbasi <x2app123@gmail.com>
2025-11-20 19:48:59 +0500

    terms

--- app/(main)/acceptTerms/index.tsx
+++ app/(main)/acceptTerms/index.tsx
@@ -24,6 +24,7 @@ const createStyles = (theme: Theme) =>
   StyleSheet.create({
     container: {
       flex: 1,
+      backgroundColor: theme.darkGreen,
     },
     backgroundImage: {
       flex: 1,
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { Redirect } from "expo-router";
-import { MAIN_ROUTES } from "@/src/constant/routes";
+import {   MAIN_ROUTES } from "@/src/constant/routes";
 
 export default function Index() {
   // return <Redirect href={`/${MAIN_ROUTES.ROLE}`} />;
-  return <Redirect href={`/(main)/dashboard`} />;
+    return <Redirect href={`/${MAIN_ROUTES.ACCEPT_TERMS}`} />;
+  // return <Redirect href={`/(main)/${MAIN_ROUTES.DASHBOARD}`} />;
 }
 
