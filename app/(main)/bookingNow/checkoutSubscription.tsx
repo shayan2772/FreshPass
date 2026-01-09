65ea615c9ae9fcffae95e123d0baa3d56c902af0
shayan abbasi <x2app123@gmail.com>
2026-01-09 20:58:15 +0500

    Update checkoutSubscription.tsx

--- app/(main)/bookingNow/checkoutSubscription.tsx
+++ app/(main)/bookingNow/checkoutSubscription.tsx
@@ -400,6 +400,8 @@ function CheckoutSubscriptionContent() {
         );
       }
 
+      console.log("paymentConfig", paymentConfig);
+
       const { error: initError } = await initPaymentSheet(paymentConfig);
 
       if (initError) {
